b48a92ae0f48daf7ae0d7db3a8b2e5fdc35f8516
bayufadayan <bayufadayan@gmail.com>
2024-06-30 12:52:46 +0700

    update saw tapi gagal

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask, jsonify, request
 import pandas as pd
 import joblib
+import requests
+import json
+import logging
 
 app = Flask(__name__)
 
-# Load model dan scaler yang sudah dilatih
+def send_data_to_laravel(data, endpoint, csrf_token):
+    url = f'http://127.0.0.1:8000/api/{endpoint}'
+    headers = {'Content-Type': 'application/json', 'X-CSRF-TOKEN': csrf_token}
+    response = requests.post(url, json={'data': data}, headers=headers)
+    return response
+
 kmeans_model = joblib.load("data/kmeans_model.pkl")
 scaler = joblib.load("data/scaler.pkl")
 
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/")
 def index():
     return "Flask API is running."
-
 
 @app.route("/process", methods=["POST"])
 def process_file():
@@ -47,8 +56,8 @@ def process_file():
             "Keaktifan",
         ]
         X = df[features]
-        X_scaled = scaler.transform(X)
-        y_kmeans = kmeans_model.predict(X_scaled)
+        x_scaled = scaler.transform(X)
+        y_kmeans = kmeans_model.predict(x_scaled)
         label_mapping = {1: 0, 0: 3, 2: 2, 3: 1}
         y_kmeans_mapped = pd.Series(y_kmeans).map(label_mapping).values
 
@@ -66,40 +75,144 @@ def process_file():
 
         return jsonify(result)
     except Exception as e:
+        logging.error(f"Failed to process file: {str(e)}")
         return f"Failed to process file: {str(e)}", 500
 
-
-@app.route('/process_daftar', methods=['POST'])
+@app.route("/process_daftar", methods=["POST"])
 def process_file_daftar():
     try:
-        file = request.files['file']
+        file = request.files["file"]
         df_daftar = pd.read_excel(file)
 
-        # Ambil data mahasiswa yang sudah diklasterisasi
-        df_clustered = pd.read_csv('data/clustered_data.csv')
+        df_clustered = pd.read_csv("data/clustered_data.csv")
 
-        # Gabungkan data pendaftar dengan data mahasiswa
         df_selection2 = pd.merge(
             df_daftar,
             df_clustered[["NPM", "Nama Mahasiswa", "Cluster", "Label Cluster"]],
             on="NPM",
             how="left",
-            validate="one_to_one"
+            validate="one_to_one",
         )
         df_selection2.rename(columns={"Cluster": "Potensi"}, inplace=True)
 
-        # Konversi ke format JSON untuk dikirimkan ke Laravel
-        result = df_selection2.to_dict(orient='records')
+        result = df_selection2.to_dict(orient="records")
 
         return jsonify(result)
     except Exception as e:
+        logging.error(f"Failed to process file: {str(e)}")
         return f"Failed to process file: {str(e)}", 500
 
+@app.route("/normalize", methods=["POST"])
+def normalize_data():
+    try:
+        criteria = get_criteria()
+        df = pd.read_csv(request.files.get("file"))
+
+        # Normalisasi data berdasarkan kriteria
+        for criteria_group, sub_criteria in criteria.items():
+            for sub_criterion, details in sub_criteria.items():
+                column_name = f"{criteria_group}_{sub_criterion}"
+                if column_name in df.columns:
+                    if details["type"] == "benefit":
+                        df[column_name] = df[column_name] / df[column_name].max()
+                    elif details["type"] == "cost":
+                        df[column_name] = df[column_name].min() / df[column_name]
+
+        # Kirim data ke Laravel untuk disimpan
+        result = df.to_dict(orient="records")
+        csrf_token = get_csrf_token()
+        logging.debug(f"Sending data to Laravel with CSRF token: {csrf_token}")
+        response = requests.post(
+            "http://127.0.0.1:8000/store-normalized-data",
+            headers={"Content-Type": "application/json", "X-CSRF-TOKEN": csrf_token},
+            data=json.dumps(result),
+        )
+        logging.debug(f"Response from Laravel: {response.status_code}, {response.text}")
+        if response.status_code != 200:
+            raise Exception(
+                f"Failed to store normalized data in Laravel: {response.text}"
+            )
+
+        return jsonify(result)
+    except Exception as e:
+        logging.error(f"Failed to normalize data: {str(e)}")
+        return f"Failed to normalize data: {str(e)}", 500
+
+@app.route("/saw", methods=["POST"])
+def process_saw():
+    try:
+        criteria = get_criteria()
+        logging.debug(f"Criteria: {criteria}")
+
+        df = pd.read_csv(request.files.get("file"))
+        logging.debug(f"Initial DataFrame: \n{df.head()}")
+
+        for criteria_group, sub_criteria in criteria.items():
+            for sub_criterion, details in sub_criteria.items():
+                column_name = f"{criteria_group}_{sub_criterion}"
+                if column_name in df.columns:
+                    df[column_name] *= details["weight"]
+
+        df["SAW_Score"] = df.sum(axis=1)
+        df["Rank"] = df["SAW_Score"].rank(ascending=False)
+
+        df.to_csv("data/saw_results.csv", index=False)
+        result = df.to_dict(orient="records")
+        logging.debug(f"SAW Results DataFrame: \n{df.head()}")
+
+        # Dapatkan CSRF token
+        csrf_token = get_csrf_token()
+
+        # Kirim data ke Laravel untuk disimpan
+        response = requests.post(
+            "http://127.0.0.1:8000/store-saw-results",
+            headers={"Content-Type": "application/json", "X-CSRF-TOKEN": csrf_token},
+            data=json.dumps(result),
+        )
+        logging.debug(f"Response from Laravel: {response.status_code}, {response.text}")
+
+        if response.status_code != 200:
+            raise Exception("Failed to store SAW results in Laravel")
+
+        return jsonify(result)
+    except Exception as e:
+        logging.error(f"Failed to process SAW: {str(e)}")
+        return f"Failed to process SAW: {str(e)}", 500
+
+def get_criteria():
+    response = requests.get("http://127.0.0.1:8000/criteria")
+    if response.status_code == 200:
+        criteria_list = response.json()
+        criteria = {}
+        for item in criteria_list:
+            criteria_group = item['criteria_group']
+            sub_criteria = item['sub_criteria']
+            if criteria_group not in criteria:
+                criteria[criteria_group] = {}
+            criteria[criteria_group][sub_criteria] = {
+                'type': item['type'],
+                'weight': float(item['weight'])
+            }
+        return criteria
+    else:
+        raise Exception("Failed to fetch criteria from Laravel")
+
+def get_csrf_token():
+    response = requests.get("http://127.0.0.1:8000/csrf-token")
+    if response.status_code == 200:
+        try:
+            csrf_token = response.json()['csrf_token']
+            logging.debug(f"CSRF Token: {csrf_token}")
+            return csrf_token
+        except (KeyError, TypeError, json.JSONDecodeError) as e:
+            logging.error("Failed to parse CSRF token from response")
+            raise Exception("Failed to parse CSRF token from response") from e
+    else:
+        raise Exception("Failed to fetch CSRF token from Laravel")
 
 def convert_grade_to_numeric(grade):
     grade_mapping = {"A": 90, "B": 80, "C": 70, "D": 60, "E": 50, "F": 40}
     return grade_mapping.get(grade, 0)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
